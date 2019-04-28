fdf04de06a31c6e110d85ba29550e426ed68cc82
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-04-28 14:12:19 -0700

    scorebar in place

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let myScore = 0;
+let highScore = 0;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="header">
+        <div className="title">Hi there! This is the clicky game!</div>
+        <div className="instructions">Start clicking to get started, just don't click the same card twice!</div>
+        <div className="scoreBar">
+          <div className="score">Current Score: {myScore}</div>
+          <div className="score">High Score: {highScore}</div>        
+        </div>
+      </div>
+    {/* Need to make a look that creates cards, each with their own ID so they can be identified easily
+    
+    Each card has an image, and an onclick function. 
+    
+    Onclick will check if it's been clicked before (boolean flag), and increment score if false. 
+
+    Increment high score if current score > high score
+
+    If alreadyClicked is true, end game by resetting current score
+    
+    Then randomize card layout.  
+    
+    */}
     </div>
   );
 }
