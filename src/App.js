305c7568d2df35ed3e8cda4de88592d2da130d73
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-04-28 15:06:40 -0700

    componentizing title bar

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 
 let myScore = 0;
 let highScore = 0;
+let title = "Hi there! This is the clicky game!";
+let instructions = "Start clicking to get started, just don't click the same card twice!";
 
 function App() {
   return (
-    <div className="App">
 
-      <div className="header">
-        <div className="title">Hi there! This is the clicky game!</div>
-        <div className="instructions">Start clicking to get started, just don't click the same card twice!</div>
-        <div className="scoreBar">
-          <div className="score">Current Score: {myScore}</div>
-          <div className="score">High Score: {highScore}</div>        
-        </div>
-      </div>
+    // structure of components:
+    // app component as entry point
+    //    title bar component
+    //      two of score component
+    //    game div
+    //      12 of card component
+    <div className="App">
+      <TitleBar title=""/>
+
     {/* Need to make a look that creates cards, each with their own ID so they can be identified easily
     
     Each card has an image, and an onclick function. 
---
+++ src/components/titleBar/titleBar.js
@@ -0,0 +1,8 @@
+<div className="header">
+<div className="title"></div>
+<div className="instructions"></div>
+<div className="scoreBar">
+  <div className="score">Current Score: {myScore}</div>
+  <div className="score">High Score: {highScore}</div>        
+</div>
+</div>
